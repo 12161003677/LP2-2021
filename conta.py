089aa76c37e2782beaf36b5c198c0653f21cfbf3
eliezer <eliezer.c.alves2015@gmail.com>
2021-08-30 22:09:28 -0300

    feat(aulas 3 e 4): explorando conceitos da POO -> encapsulamento, metodo construtor, property e static methods

--- conta.py
+++ conta.py
@@ -1,20 +1,55 @@
 class Conta:
+    __codigoBanco = '001'
 
     def __init__(self, numero, titular, saldo, limite):
 
-        # atributos publicos
-        self.numero = numero
-        self.titular = titular
-        self.limite = limite
-
-        # atributos privados
+        self.__numero = numero
+        self.__titular = titular
+        self.__limite = limite
         self.__saldo = saldo
 
     def imprimirSaldo(self):
-        print("Saldo de {} do titular {}".format(self.__saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
 
     def depositar(self, valor):
         self.__saldo += valor
 
     def sacar(self, valor):
         self.__saldo -= valor
+
+    # getters e setters
+    def getNumero(self):
+        return self.__numero
+
+    def setNumero(self, numero):
+        self.__numero = numero
+
+    def getTitular(self):
+        return self.__titular
+
+    def setTitular(self, titular):
+        self.__titular = titular
+
+    def getLimite(self):
+        return self.__limite
+
+    def setLimite(self, limite):
+        self.__limite = limite
+
+    # properties
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    # métodos estáticos
+    @staticmethod
+    def codigBanco():
+        return Conta.__codigoBanco
--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from conta import Conta
 
 if __name__ == '__main__':
     conta1 = Conta(199249, 'ELIEZER ALVES', 250, 5000.00)
-
     conta1.imprimirSaldo()
     conta1.sacar(100)
     conta1.imprimirSaldo()
@@ -11,9 +10,28 @@ if __name__ == '__main__':
     conta1.depositar(5000)
     conta1.imprimirSaldo()
 
-    conta2 = Conta(199234, 'CHAPOLIM COLORADO', 20300, 3000.00)
+    conta2 = Conta(199234, 'CHAPOLIM COLORIDO', 20300, 3000.00)
     conta2.imprimirSaldo()
     conta2.sacar(155)
     conta2.imprimirSaldo()
     conta2.depositar(1500)
     conta2.imprimirSaldo()
+
+    # manipulação do limite da conta 2
+    print("O limite da conta é {}".format(conta2.getLimite()))
+    conta2.setLimite(5000)
+    print("O limite da conta é {}".format(conta2.getLimite()))
+
+    # manipulação do titular da conta 2
+    print("O titular da conta é {}".format(conta2.getTitular()))
+    conta2.setTitular('CHAPOLIN COLORADO')
+    print("O titular da conta é {}".format(conta2.getTitular()))
+
+    # acessando as properties do objeto
+    print(conta2.limite)
+    conta2.limite = 8500
+    print(conta2.limite)
+    print(conta2.saldo)
+
+    # acessando os métodos estáticos da classe
+    print("Código do banco: {}".format(Conta.codigBanco()))
