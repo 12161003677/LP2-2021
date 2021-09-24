6eb5664e9f348581d74ae6887c0636f03f500ab6
eliezer <eliezer.c.alves2015@gmail.com>
2021-09-23 21:16:37 -0300

    feat(aula 12): iniciando processo de crud

--- jogoteca/main.py
+++ jogoteca/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,20 +8,37 @@ class Jogo:
         self._categoria = categoria
         self._console = console
 
-@app.route("/")
+jogo1 = Jogo('Tetrix', 'Puzzle', 'Super Nintendo')
+jogo2 = Jogo('Super Mario', 'Aventura', 'Nintendo 64')
+jogo3 = Jogo('Sonic', 'Aventura', 'Mega Drive')
+jogo4 = Jogo('Sonic2', 'Aventura', 'Mega Drive')
+jogo5 = Jogo('Sonic3', 'Aventura', 'Mega Drive')
 
+jogos = [jogo1, jogo2, jogo3, jogo4, jogo5]
+
+@app.route("/")
 def index():
     
     titulo = "Lista de Jogos"
-    jogo1 = Jogo('Tetrix', 'Puzzle', 'Super Nintendo')
-    jogo2 = Jogo('Super Mario', 'Aventura', 'Nintendo 64')
-    jogo3 = Jogo('Sonic', 'Aventura', 'Mega Drive')
-    jogo4 = Jogo('Sonic2', 'Aventura', 'Mega Drive')
-    jogo5 = Jogo('Sonic3', 'Aventura', 'Mega Drive')
-
-    jogos = [jogo1, jogo2, jogo3, jogo4, jogo5]
 
     return render_template('lista.html', titulo = titulo, jogos = jogos)
-    
+
+@app.route("/create")
+def create():
+    titulo = "Novo Jogo"
+    return render_template('novo.html', titulo = titulo)
+
+
+@app.route("/store", methods=['POST'])
+def store():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+
+    novoJogo = Jogo(nome, categoria, console)
+    jogos.append(novoJogo)
+
+    return index()
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
