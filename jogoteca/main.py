29d097d5e3660fc1d9c217c96b3aacdbb48d28a3
eliezer <eliezer.c.alves2015@gmail.com>
2021-09-28 23:11:56 -0300

    feat(aula 15): evoluindo a funcionalidade de login / criando validacao de sessao para as rotas existentes

--- jogoteca/main.py
+++ jogoteca/main.py
@@ -9,6 +9,12 @@ class Jogo:
         self._categoria = categoria
         self._console = console
 
+class Usuario:
+    def __init__(self, username, nome, password):
+        self._username = username
+        self._nome = nome
+        self._password = password
+
 jogo1 = Jogo('Tetrix', 'Puzzle', 'Super Nintendo')
 jogo2 = Jogo('Super Mario', 'Aventura', 'Nintendo 64')
 jogo3 = Jogo('Sonic', 'Aventura', 'Mega Drive')
@@ -17,22 +23,37 @@ jogo5 = Jogo('Sonic3', 'Aventura', 'Mega Drive')
 
 jogos = [jogo1, jogo2, jogo3, jogo4, jogo5]
 
+usuario1 = Usuario('eli', 'Eliezer Alves', 'admin')
+usuario2 = Usuario('admin', 'Administrador', 'admin')
+
+usuarios = {
+    usuario1._username: usuario1,
+    usuario2._username: usuario2,
+}
+
+def session_valid():
+    if 'user_logged' not in session or session['user_logged'] == None:
+        return False
+    return True
+
 @app.route("/login")
 def login():
+    callback_url = request.args.get('callback_url') or ""
 
-    return render_template('login.html')
+    return render_template('login.html', callback_url=callback_url)
 
 @app.route("/auth", methods=['POST'])
 def auth():
 
-    if 'mestra' == request.form['senha']:
-        session['user_logged'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        return redirect('/')
+    if request.form['username'] in usuarios:
+        if usuarios[request.form['username']]._password == request.form['password']:
+            session['user_logged'] = request.form['username']
+            flash(request.form['username'] + ' logou com sucesso!')
+            next_page = request.form['callback_url']
+            return redirect("/{}".format(next_page))
     
-    else:
-        flash('Falha ao realizar login para ' + request.form['usuario'] + '!')
-        return redirect('/login')
+    flash('Falha ao realizar login para ' + request.form['username'] + '!')
+    return redirect('/login')
 
 @app.route("/logout")
 def logout():
@@ -44,19 +65,26 @@ def logout():
 
 @app.route("/")
 def index():
+    if not session_valid():
+        return redirect('/login?callback_url=')
     
     titulo = "Lista de Jogos"
-
     return render_template('lista.html', titulo = titulo, jogos = jogos)
 
 @app.route("/create")
 def create():
+    if not session_valid():
+        return redirect('/login?callback_url=create')
+
     titulo = "Novo Jogo"
     return render_template('novo.html', titulo = titulo)
 
 
 @app.route("/store", methods=['POST'])
 def store():
+    if not session_valid():
+        return redirect('/login?callback_url=create')
+
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
